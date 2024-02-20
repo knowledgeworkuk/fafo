326b7c4f6fd294a2b70a38115ea95d5073a751d7
Andy Carlier <Andy.Carlier@hdgroupuk.com>
2024-02-20 21:22:12 +0000

    added site warmup package project

---
+++ FAFO/SiteWarmupDashboard/Controllers/SiteWarmupApiController.cs
@@ -0,0 +1,32 @@
+﻿using KnowledgeWork.SiteWarmupDashboard.Services;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.BackOffice.Controllers;
+using Umbraco.Cms.Web.Common;
+
+namespace KnowledgeWork.SiteWarmupDashboard.Controllers
+{
+    public class SiteWarmupApiController : UmbracoAuthorizedApiController
+    {
+
+        private UmbracoHelper _helper;
+        private IContentService _contentService;
+
+        public SiteWarmupApiController(UmbracoHelper umbracoHelper, IContentService contentService)
+        {
+            _helper = umbracoHelper;
+            _contentService = contentService;
+        }
+
+
+        [HttpGet]
+        public JsonResult GetPages()
+        {
+            var contentService = new ContentAdminService(_helper);
+
+            var pages = contentService.GetPageList();
+
+            return new JsonResult(pages);
+        }
+    }
+}
---
+++ FAFO/SiteWarmupDashboard/Services/ContentAdminService.cs
@@ -0,0 +1,49 @@
+﻿using KnowledgeWork.SiteWarmupDashboard.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Web.Common;
+using Umbraco.Extensions;
+using static Umbraco.Cms.Core.Constants.HttpContext;
+
+namespace KnowledgeWork.SiteWarmupDashboard.Services
+{
+    public class ContentAdminService
+    {
+
+        private UmbracoHelper _helper;
+
+        public ContentAdminService(UmbracoHelper umbracoHelper)
+        {
+            _helper = umbracoHelper;
+        }
+
+        public List<PageViewModel> GetPageList()
+        {
+            var pages = new List<PageViewModel>();
+
+            var root = _helper.ContentAtRoot().FirstOrDefault();
+
+            if (root != null)
+            {
+                pages.Add(new PageViewModel(root.Id,root.Name, root.Url(null, UrlMode.Absolute)));
+                TraversePageList(root, ref pages);
+            }
+
+            return pages;
+        }
+
+        private void TraversePageList(IPublishedContent node, ref List<PageViewModel> pages) {
+
+            if (node.Children.Any()) {
+
+                foreach (var item in node.Children)
+                {
+                    pages.Add(new PageViewModel(item.Id, item.Name, item.Url(null, UrlMode.Absolute)));
+                    
+                    TraversePageList(item, ref pages);
+                }            
+            }        
+        }
+    }
+}
